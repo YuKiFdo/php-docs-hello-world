e8c29acee952209fd37bfa971fd128fec02bd594
Shehal Herath <100369558+YuKiFdo@users.noreply.github.com>
2024-06-06 00:08:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,20 @@
-<?php
 <?php include('includes/header.php'); ?>
 
-    <div class="py-5" style="background-image: url('assets/image/pos.jpg'); background-size: cover;">
-        <div class="container my-5">
-            <div class="row">
-                <div class="col-md-12 py-5 text-center">
+<div class="py-5" style="background-image: url('assets/image/pos.jpg'); background-size: cover;">
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-md-12 py-5 text-center">
 
-                    <?php alertMessage(); ?>
+                <?php alertMessage(); ?>
 
-                    <h1 class="mt-3" style="color: white">Sakura Mobile</h1>
+                <h1 class="mt-3" style="color: white">Sakura Mobile</h1>
 
-                    <?php if(!isset($_SESSION['loggedIn'])) : ?>
-                    <a href="login.php" class="btn btn-primary mt-4">Login</a>
-                    <?php endif; ?>
-                </div>
+                <?php if (!isset($_SESSION['loggedIn'])) : ?>
+                <a href="login.php" class="btn btn-primary mt-4">Login</a>
+                <?php endif; ?>
             </div>
         </div>
     </div>
+</div>
 
 <?php include('includes/footer.php'); ?>
