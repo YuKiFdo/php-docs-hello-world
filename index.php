17395fc4c528b8664d17dab76c5eb58d03ec1c28
YuKi <yukifernando0001@gmail.com>
2024-06-07 07:25:36 +0530

    test piush

--- index.php
+++ index.php
@@ -1,20 +1,5 @@
-<?php include('includes/header.php'); ?>
+<?php
 
-<div class="py-5" style="background-image: url('assets/image/pos.jpg'); background-size: cover;">
-    <div class="container my-5">
-        <div class="row">
-            <div class="col-md-12 py-5 text-center">
+    echo "Hello World";
 
-                <?php alertMessage(); ?>
 
-                <h1 class="mt-3" style="color: white">Sakura Mobile</h1>
-
-                <?php if (!isset($_SESSION['loggedIn'])) : ?>
-                <a href="login.php" class="btn btn-primary mt-4">Login</a>
-                <?php endif; ?>
-            </div>
-        </div>
-    </div>
-</div>
-
-<?php include('includes/footer.php'); ?>
