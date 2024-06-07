a6da29bcbb8a671c9fe5503a6385c28385b8857b
YuKi <yukifernando0001@gmail.com>
2024-06-07 08:20:31 +0530

    test

--- config/dbconnection.php
+++ config/dbconnection.php
@@ -1,17 +1,17 @@
 <?php
-
-define('DB_SERVER', "your_server_name.database.windows.net"); // Replace with your server name
-define('DB_USERNAME', "your_username"); // Replace with your username
-define('DB_PASSWORD', "your_password"); // Replace with your password
-define('DB_DATABASE', "dbsakura_mobile"); // Replace with your database name
-
-// Connection info array
-$connectionInfo = array("Database" => DB_DATABASE, "UID" => DB_USERNAME, "PWD" => DB_PASSWORD);
-
-// Establish the connection
-$conn = sqlsrv_connect(DB_SERVER, $connectionInfo);
-if(!$conn){
-    die("Connection Failed: ". mysqli_connect_error());
+try {
+    $conn = new PDO("sqlsrv:server = tcp:sakurampos.database.windows.net,1433; Database = sakurampos", "sakurampos", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
 }
 
-?>
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "sakurampos", "pwd" => "{your_password_here}", "Database" => "sakurampos", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:sakurampos.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+if (!$conn) {
+    die("Connection Failed: " . mysqli_connect_error());
+}
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-phpinfo()
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+require 'config/function.php'; // Ensure this path is correct
 ?>
 <!DOCTYPE html>
 <html lang="en">
