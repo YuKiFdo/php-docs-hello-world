5ce839af717f76763ee2c250e793bdf09cd2f7b4
YuKi <yukifernando0001@gmail.com>
2024-06-07 07:43:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-require '*/config/function.php'; // Ensure this path is correct
+require 'config/function.php'; // Ensure this path is correct
 ?>
 <!DOCTYPE html>
 <html lang="en">
