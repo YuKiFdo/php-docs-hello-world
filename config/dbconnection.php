c7eac7ca3d2d35d9ef3be33b480df400e0240597
YuKi <yukifernando0001@gmail.com>
2024-06-07 08:53:30 +0530

    Update dbconnection.php

--- config/dbconnection.php
+++ config/dbconnection.php
@@ -4,10 +4,8 @@ define('DB_SERVER',"sakurampos.mysql.database.azure.com");
 define('DB_USERNAME',"sakurampos");
 define('DB_PASSWORD',"Yasas1@22");
 define('DB_DATABASE',"dbsakura_mobile");
-// ssl require
-define('MYSQLI_CLIENT_SSL', MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
 
-$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE, MYSQLI_CLIENT_SSL);
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE, 3306);
 
 if(!$conn){
     die("Connection Failed: ". mysqli_connect_error());
